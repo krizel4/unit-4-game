4280532f2a7b1fa13474154375d6b989ad80e249
Krizel Minnema <krizelminnema@gmail.com>
2019-10-30 20:55:10 -0700

    setting up game with pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// Variables
+var wins = 0;
+var losses = 0;
+var computer = 0;
+var points = 0;
+
+// Random number to match
+	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+// Random number assignments to 4 gems.
+	
+	
+// The following are conditional statements to log the wins, losses and points.
+
+// This is the body of the game and where the wins, losses, computer number and points.
+        var gameBody = 
+        "<p>Gem Goal: " + computer + "</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + guessesLeft + "</p>" +
+        "<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
+
+        document.querySelector("#game").innerHTML = gameBody;
