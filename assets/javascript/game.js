61d6a52be4dead776f023d67b47ceab468e5ba72
Krizel Minnema <krizelminnema@gmail.com>
2019-11-02 11:48:37 -0700

    restructuring bones of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,12 @@
-// Variables
+// Variables 
+var wins = 0;
+var losses = 0;
+var computerNumber = "0";
+var userScore = "0";
 
-// Randomized number to meet
+// Randomized computer number for user to match
+var randomNumber = Math.floor(Math.random()*12) + 1;
+$("#computerNumber").html(randomNumber);
 
 // Randomized number for each gem
  
